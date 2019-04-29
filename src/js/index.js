66220501fa4cf2ff1702d59a844430cf0bbe6a9b
seoksanghwan <bdoi16@naver.com>
2019-04-29 12:19:46 +0900

    ie9 event add & modify

--- src/js/index.js
+++ src/js/index.js
@@ -2,18 +2,17 @@ import "@babel/polyfill";
 import '../styles/common.scss';
 
 // /*star rating UI Js */
-const wrap = document.querySelector('#wrap');
-var onePoint = document.querySelector('.one-point');
-var twoPoint = document.querySelector('.two-point');
-var fourPoint = document.querySelector('.four-point');
-var starUiOne = onePoint.querySelectorAll('.star-point .star-raiting button');
-var starUiTwo = twoPoint.querySelectorAll('.star-point .star-raiting button');
-var starUiFour = fourPoint.querySelectorAll('.star-point .star-raiting button');
+const onePoint = document.querySelector('.one-point');
+const twoPoint = document.querySelector('.two-point');
+const fourPoint = document.querySelector('.four-point');
+const starUiOne = onePoint.querySelectorAll('.star-point .star-raiting button');
+const starUiTwo = twoPoint.querySelectorAll('.star-point .star-raiting button');
+const starUiFour = fourPoint.querySelectorAll('.star-point .star-raiting button');
 
-var starPoint = function (event, li) {
-  var evpar = event.target;
-  var grade = Number(evpar.getAttribute('data-grade'));
-  var active = document.querySelector('.active');
+const starPoint = (event, li) => {
+  const evpar = event.target;
+  const grade = Number(evpar.getAttribute('data-grade'));
+  const active = document.querySelector('.active');
   if (evpar.tagName === 'BUTTON') {
     for (let i = 0; i < li.length; i++) {
       if (i <= grade) {
@@ -25,41 +24,42 @@ var starPoint = function (event, li) {
   }
 }
 
-onePoint.addEventListener('click', function (event) { return starPoint(event, starUiOne) });
-twoPoint.addEventListener('click', function (event) { return starPoint(event, starUiTwo) });
-fourPoint.addEventListener('click', function (event) { return starPoint(event, starUiFour) });
+onePoint.addEventListener('click', () => { return starPoint(event, starUiOne) });
+twoPoint.addEventListener('click', () => { return starPoint(event, starUiTwo) });
+fourPoint.addEventListener('click', () => { return starPoint(event, starUiFour) });
 
 /*Textarea UI Js */
-var boxSection = document.querySelector('.caption-box .default-box')
-var defaultTextarea = boxSection.querySelector('.box-section #textUi');
-var defaultEm = boxSection.querySelector('.box-section em');
-var saveButton = boxSection.querySelector('button');
-var readonlySection = document.querySelector('.readonly-box');
-var readOnlyTextarea = readonlySection.querySelector('#textUiRead');
-var readOnlyEm = readonlySection.querySelector('em');
-var disabelSection = document.querySelector('.disabel-box');
-var disabelTextarea = disabelSection.querySelector('#textUiDis');
-var disabelEm = disabelSection.querySelector('em');
-var textLimit = Number(defaultTextarea.getAttribute('maxLength'));
+const boxSection = document.querySelector('.caption-box .default-box')
+const defaultTextarea = boxSection.querySelector('.box-section #textUi');
+const defaultEm = boxSection.querySelector('.box-section em');
+const saveButton = boxSection.querySelector('button');
+const readonlySection = document.querySelector('.readonly-box');
+const readOnlyTextarea = readonlySection.querySelector('#textUiRead');
+const readOnlyEm = readonlySection.querySelector('em');
+const disabelSection = document.querySelector('.disabel-box');
+const disabelTextarea = disabelSection.querySelector('#textUiDis');
+const disabelEm = disabelSection.querySelector('em');
+const textLimit = Number(defaultTextarea.getAttribute('maxLength'));
 
 defaultEm.innerText = textLimit;
 readOnlyEm.innerText = textLimit;
 disabelEm.innerText = textLimit;
 
-var textValue, textLenght, orderStorage;
+let textValue, textLenght, orderStorage;
 
-var ieLowChecker = navigator.userAgent.indexOf('9.0') !== -1 ? true : false;
+const ieLowChecker = navigator.userAgent.indexOf('9.0') !== -1 ? true : false;
 
+/*IE9 PlaceHolder Event*/
 if (ieLowChecker) {
-  var areaText = document.createElement('span');
+  const areaText = document.createElement('span');
   areaText.innerText = '내용이 있을수도 있습니다.';
   areaText.classList.add('placeHolder');
 
-  var areaText_readOnly = document.createElement('span');
+  const areaText_readOnly = document.createElement('span');
   areaText_readOnly.innerText = '주문 요청사항을 입력해주세요. 읽기전용 입니다.';
   areaText_readOnly.classList.add('placeHolder');
 
-  var areaText_disabled = document.createElement('span');
+  const areaText_disabled = document.createElement('span');
   areaText_disabled.innerText = '주문 요청사항을 입력해주세요. 비활성화 상태입니다.';
   areaText_disabled.classList.add('placeHolder');
 
@@ -67,33 +67,35 @@ if (ieLowChecker) {
   readonlySection.appendChild(areaText_readOnly);
   disabelSection.appendChild(areaText_disabled);
 
-  readOnlyTextarea.addEventListener('focusin', function () {
-    areaText.classList.add('active');
-    areaText_readOnly.classList.add('active');
-  });
-  readOnlyTextarea.addEventListener('focusout', function () {
-    areaText.classList.add('active');
-    areaText_readOnly.classList.remove('active');
-  });
+  const focusInOutEvent = element => {
+    element.addEventListener('focusin', () => {
+      element.name === 'textUi' ? (defaultTextarea.value.length <= 0 ? areaText.classList.add('active') : false) : areaText_readOnly.classList.add('active');
+    });
+    element.addEventListener('focusout', () => {
+      element.name === 'textUi' ? (defaultTextarea.value.length <= 0 ? areaText.classList.remove('active') : false) : areaText_readOnly.classList.remove('active');
+    });
+  }
+
+  focusInOutEvent(defaultTextarea);
+  focusInOutEvent(readOnlyTextarea);
 }
 
-var orderOptionEvent = function () {
+const orderOptionEvent = () => {
   textValue = defaultTextarea.value;
   textLenght = textValue.length;
-  var lengthText = Number(textLimit) - Number(textValue.length);
+  const lengthText = Number(textLimit) - Number(textValue.length);
+
   if (textLenght > 0) {
-    ieLowChecker ? areaText.classList.add('active') : null;
     saveButton.classList.add('active');
     defaultTextarea.classList.add('active');
     defaultTextarea.parentElement.parentElement.classList.add('active');
   } else {
-    ieLowChecker ? areaText.classList.remove('active') : null;
-    ieLowChecker ? areaText_readOnly.classList.remove('active') : null;
     saveButton.classList.remove('active');
     defaultTextarea.classList.remove('active');
     defaultTextarea.parentElement.parentElement.classList.remove('active');
     readOnlyEm.innerText = textLimit - textValue.length;
   }
+
   defaultEm.innerText = lengthText > 0 ? lengthText : 0;
   defaultTextarea.value = defaultTextarea.value.slice(0, textLimit);
 }
